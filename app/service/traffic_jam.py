f82c8824619fb3c85065a4f52ed8759d72abb056
Yair <ytykhn8@gmail.com>
2024-12-10 18:35:57 +0200

    pushed by Simcha 12-10-24 18:35

--- app/service/traffic_jam.py
+++ app/service/traffic_jam.py
@@ -6,13 +6,14 @@ from app.dbs.redis.redis_traffic_repository import get_traffic_jam_by_points
 
 def find_traffic_jam_in_route(steps: List[Dict[str, List[int]]]):
     coordinates_str = [f"{coord[0]},{coord[1]}" for item in steps for coord in [item['coordinates']]]
-    return filter(lambda x: x, [get_traffic_jam_by_points(x) for x in coordinates_str])
+    return list(filter(lambda x: x, [get_traffic_jam_by_points(x) for x in coordinates_str]))
 
 
 def find_short_route_by_traffic_jam(routes: Dict[str, List[Dict[str, List[int]]]]):
     def sum_duration(accumulator, item):
-        print("a")
         return accumulator + int(item.get('duration', 0))
 
-    duration_routing = {k: reduce(sum_duration, find_traffic_jam_in_route(v["steps"]), 0) for k, v in routes.items()}
-    return min(duration_routing,key=duration_routing.get)
+    traffic_jam = {k: find_traffic_jam_in_route(v["steps"]) for k, v in routes.items()}
+    print("a")
+    # duration_routing = {k:reduce(sum_duration,traffic_jam}
+    # return min(duration_routing,key=duration_routing.get)
